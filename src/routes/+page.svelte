ea2a12598052f0098a686bc557b57b789096cad3
Abhay Raj Singh <abhayonlyone@gmail.com>
2023-09-09 03:45:05 +0530

    feat: hero

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,25 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="hero min-h-screen bg-base-200">
+	<div class="hero-content">
+		<div class="max-w-screen-lg">
+			<div>
+				<h1 class="text-5xl font-bold sm:inline mr-4">Abhay Raj Singh.</h1>
+				<a
+					class="text-3xl opacity-80 tooltip hover:underline"
+					data-tip="International Phonetic Alphabet"
+					href="https://en.wikipedia.org/wiki/International_Phonetic_Alphabet"
+				>
+					/ʌbʰj raːdʒ sɪŋh/
+				</a>
+			</div>
+			<p class="text-2xl pt-6 font-[Rwundu]">
+				An engineer who loves to build things. Specializing in <b>backend engineering</b>, and
+				aiming for proficiency in <em>all-stack</em>, a fancy way of saying
+				<a href="https://abhay.rs/blog">everything engineering</a>.
+			</p>
+			<p class="text-2xl pt-6 font-[Rwundu]">
+				Distinctive skills include debugging, problem solving, and simplification. Distinctive
+				traits include curiosity, creativity, and critical thinking.
+			</p>
+		</div>
+	</div>
+</div>
