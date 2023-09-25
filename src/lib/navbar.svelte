17d1cb81d900995c2d86202995a718c871820f27
Abhay Raj Singh <abhayonlyone@gmail.com>
2023-09-26 01:11:30 +0530

    fix: page switch fix

--- src/lib/navbar.svelte
+++ src/lib/navbar.svelte
@@ -19,10 +19,13 @@
 		class="absolute z-10 top-0 left-1/2 -translate-x-1/2 transition-colors -translate-y-[84%] w-[200%] aspect-square rounded-full bg-base-200"
 	/>
 	<ul
-		class="relative flex text-xl z-20 justify-center -0 w-full [&>li:not(:last-child)]:mr-12 [&>*]transition-all [&>*]:duration-150"
+		class="relative flex text-2xl z-20 justify-center -0 w-full [&>li:not(:last-child)]:mr-12 [&>*]transition-all [&>*]:duration-150"
 	>
 		{#each PAGES as page}
-			<li class={currentPath === page.href ? 'text-3xl hover:scale-110' : 'hover:scale-125'}>
+			<li
+				class:hover:scale-110={currentPath !== base + page.href}
+				class:scale-125={currentPath === base + page.href}
+			>
 				<h3>
 					<a href={base + page.href}>{page.name}</a>
 				</h3>
