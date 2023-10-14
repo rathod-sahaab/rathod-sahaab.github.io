c34f581bb9be0add13833cf58689d5b5d1bb7ff6
Abhay Raj Singh <abhayonlyone@gmail.com>
2023-10-15 03:53:16 +0530

    socials

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,9 @@
 <script>
+	import Socials from './about/socials.svelte'
 	import Timeline from './about/timeline.svelte'
 	import Title from './about/title.svelte'
 </script>
 
 <Title />
+<Socials />
 <Timeline />
---
+++ src/routes/about/socials.svelte
@@ -0,0 +1,43 @@
+<script>
+	import GithubLooper from '~icons/line-md/github-loop'
+	import Linkedin from '~icons/devicon/linkedin'
+	import Twitter from '~icons/devicon/twitter'
+</script>
+
+<section class="py-12 bg-base-100 flex justify-center">
+	<div class="max-w-screen-md w-full">
+		<div class="text-center">
+			<h3 class="text-3xl">Socials.</h3>
+			<span class="text-xl opacity-80 tooltip hover:underline"> /sō′shəls/ </span>
+		</div>
+		<ul
+			class="p-4 flex flex-row justify-center items-center [&>*:not(:last-child)]:mr-8 [&>*]:inline-block"
+		>
+			<li>
+				<a
+					href="https://github.com/rathod-sahaab"
+					target="_blank"
+					rel="noopener noreferrer"
+					class="text-2xl font-bold hover:underline"><GithubLooper /></a
+				>
+			</li>
+			<li>
+				<a
+					href="https://linkedin.com/in/rathod-sahaab"
+					target="_blank"
+					rel="noopener noreferrer"
+					class="text-2xl font-bold hover:underline"><Linkedin /></a
+				>
+			</li>
+			<li>
+				<a
+					href="https://twitter.com/swayambhu_dev"
+					target="_blank"
+					rel="noopener noreferrer"
+					class="text-2xl font-bold hover:underline"><Twitter /></a
+				>
+			</li>
+		</ul>
+		<div />
+	</div>
+</section>
